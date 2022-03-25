3f570ce8e0921684ccaa13db4dfbb4aca2010981
quavious <leenowon97@gmail.com>
2022-03-25 23:29:00 +0900

    Fix: Add Comments Unused files

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,6 +1,6 @@
-export { default as normal } from '../../natsuki/natsuki-normal.png';
-export { default as move } from '../../natsuki/natsuki-move.png';
-export { default as stop } from '../../natsuki/natsuki-stop.png';
+// export { default as normal } from '../../natsuki/natsuki-normal.png';
+// export { default as move } from '../../natsuki/natsuki-move.png';
+// export { default as stop } from '../../natsuki/natsuki-stop.png';
 
 export { default as cat } from '../../cats/cat.png';
 
