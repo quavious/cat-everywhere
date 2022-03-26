12a703d8b71862b89d43b5d7de167e41c70f3075
quavious <leenowon97@gmail.com>
2022-03-26 20:33:52 +0900

    Fix: Service Worker Registration

--- public/sw.js
+++ public/sw.js
@@ -5,10 +5,21 @@ async function install() {
   const cache = await caches.open(version);
   await cache.addAll(manifest);
 }
-addEventListener('install', (e) => e.waitUntil(install()));
 
 async function activate() {
   const keys = await caches.keys();
   await Promise.all(keys.map((key) => key !== version && caches.delete(key)));
 }
-addEventListener('activate', (e) => e.waitUntil(activate()));
+
+self.addEventListener('install', (e) => e.waitUntil(install()));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      if (resp) {
+        return resp;
+      }
+      return fetch(event.request);
+    }),
+  );
+});
+self.addEventListener('activate', (e) => e.waitUntil(activate()));
--- src/index.tsx
+++ src/index.tsx
@@ -15,3 +15,28 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('app'),
 );
+
+const registerServiceWorker = async () => {
+  if ('serviceWorker' in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register(new URL(`${window.location.origin}/sw.js`), {
+        type: 'module',
+      });
+      if (process.env.NODE_ENV === 'development') {
+        if (registration.installing) {
+          console.log('Service Worker Installing');
+        } else if (registration.waiting) {
+          console.log('Service Worker Installed');
+        } else if (registration.active) {
+          console.log('Service Worker Active');
+        }
+      }
+    } catch (err) {
+      if (process.env.NODE_ENV === 'development') {
+        console.error(`Registration Failed with ${err}`);
+      }
+    }
+  }
+};
+
+registerServiceWorker();
